15e957e2be097f5acd964b81cb701c60be1919dd
Gordan Blazic <gordanus@gmail.com>
2019-10-10 19:08:49 +0200

    styles update

---
+++ script.js
@@ -0,0 +1,36 @@
+function lekcijaDva() {
+    var password = prompt("Unesite lozinku da biste preuzeli fajl:");
+    if (password === "excelguru") {
+        window.open("Lekcija 2.zip")
+    } else {
+        alert("Neispravna lozinka, probajte ponovo!");
+    }
+}
+
+
+function lekcijaTri() {
+    var password = prompt("Unesite lozinku da biste preuzeli fajl:");
+    if (password === "excelninja") {
+        window.open("Lekcija 3.zip")
+    } else {
+        alert("Neispravna lozinka, probajte ponovo!")
+    }
+}
+
+function lekcijaCetiri() {
+    var password = prompt("Unesite lozinku da biste preuzeli fajl:");
+    if (password === "excelexpert") {
+        window.open("Lekcija 4.zip")
+    } else {
+        alert("Neispravna lozinka, probajte ponovo!")
+    }
+}
+
+function lekcijaPet() {
+    var password = prompt("Unesite lozinku da biste preuzeli fajl:");
+    if (password === "sundjerbob") {
+        window.open("Lekcija 5.zip")
+    } else {
+        alert("Neispravna lozinka, probajte ponovo!")
+    }
+}
