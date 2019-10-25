72887b9b579f2c5e1e9a977b26f652e74ef001df
Gordan Blazic <gordanus@gmail.com>
2019-10-25 19:05:11 +0200

    script fix

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ function lekcijaDva() {
     var password = prompt("Unesite lozinku da biste preuzeli fajl:");
     if (password === "excelguru") {
         window.open("Lekcija 2.zip")
+    } else if (password === null) {
+        return;
     } else {
         alert("Neispravna lozinka, probajte ponovo!");
     }
@@ -12,8 +14,10 @@ function lekcijaTri() {
     var password = prompt("Unesite lozinku da biste preuzeli fajl:");
     if (password === "excelninja") {
         window.open("Lekcija 3.zip")
+    } else if (password === null) {
+        return;
     } else {
-        alert("Neispravna lozinka, probajte ponovo!")
+        alert("Neispravna lozinka, probajte ponovo!");
     }
 }
 
@@ -21,8 +25,10 @@ function lekcijaCetiri() {
     var password = prompt("Unesite lozinku da biste preuzeli fajl:");
     if (password === "excelexpert") {
         window.open("Lekcija 4.zip")
+    } else if (password === null) {
+        return;
     } else {
-        alert("Neispravna lozinka, probajte ponovo!")
+        alert("Neispravna lozinka, probajte ponovo!");
     }
 }
 
@@ -30,7 +36,9 @@ function lekcijaPet() {
     var password = prompt("Unesite lozinku da biste preuzeli fajl:");
     if (password === "sundjerbob") {
         window.open("Lekcija 5.zip")
+    } else if (password === null) {
+        return;
     } else {
-        alert("Neispravna lozinka, probajte ponovo!")
+        alert("Neispravna lozinka, probajte ponovo!");
     }
 }
