df52eba9114833a2883c500b0d44abdd5f924cfa
Gordan Blažić <gblazic@smart.rs>
2020-10-02 12:45:06 +0200

    new exercises

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-function lekcijaDva() {
+function resenja() {
     var password = prompt("Unesite lozinku da biste preuzeli fajl:");
     if (password === "excelguru") {
-        window.open("Lekcija 2.zip")
+        window.open("Resenja.zip")
     } else if (password === null) {
         return;
     } else {
@@ -9,36 +9,5 @@ function lekcijaDva() {
     }
 }
 
-
-function lekcijaTri() {
-    var password = prompt("Unesite lozinku da biste preuzeli fajl:");
-    if (password === "excelninja") {
-        window.open("Lekcija 3.zip")
-    } else if (password === null) {
-        return;
-    } else {
-        alert("Neispravna lozinka, probajte ponovo!");
-    }
-}
-
-function lekcijaCetiri() {
-    var password = prompt("Unesite lozinku da biste preuzeli fajl:");
-    if (password === "excelexpert") {
-        window.open("Lekcija 4.zip")
-    } else if (password === null) {
-        return;
-    } else {
-        alert("Neispravna lozinka, probajte ponovo!");
-    }
-}
-
-function lekcijaPet() {
-    var password = prompt("Unesite lozinku da biste preuzeli fajl:");
-    if (password === "sundjerbob") {
-        window.open("Lekcija 5.zip")
-    } else if (password === null) {
-        return;
-    } else {
-        alert("Neispravna lozinka, probajte ponovo!");
-    }
-}
+// dogadjaj za dugmad
+// onclick="lekcijaDva()"
